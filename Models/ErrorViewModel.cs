fc60f8e0a51cf23f301350e248e8ef2ee47dafdd
Ayşe Başak Demir <ayse.demir1@boun.edu.tr>
2021-05-30 13:45:28 +0300

    initial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace todoHW.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/ToDo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace todoHW.Models
+{
+    public class ToDo
+    {
+        public int ID { get; set; }
+
+        public string Title { get; set; }
+
+        public bool isCompleted { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        public DateTime CompletedDate { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public int CategoryID { get; set; }
+
+        public Category Category { get; set; }
+
+    }
+}
